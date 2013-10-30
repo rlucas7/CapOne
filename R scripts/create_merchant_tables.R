b8706052059148b712924b30970fb60852096455
Lucas Roberts <lucasroberts@Lucas-Robertss-MacBook-Pro-5.local>
2013-10-29 20:52:45 -0400

    added a file that has my sweet shit motherfucker

---
+++ R scripts/create_merchant_tables.R
@@ -0,0 +1,28 @@
+### 
+###	Code that writes tables for each specific merchant iterating across 
+### all merchants and 24 tables creating 14 tables
+### where one table is all the information for each specific merchants 
+###
+###
+###
+###========================================================
+
+
+
+install.packages("RMySQL") 
+library(RMySQL)
+drv <- dbDriver("MySQL")
+mydb <- dbConnect(drv, user='andy', password='andy', dbname='mysql', host='128.173.212.125')
+dbListTables(mydb)
+dbListTables(mydb, "cap1 disk1 build_auth_155")
+relevant_ids <- dbGetQuery(mydb, "SELECT count(distinct acct_id_code) from mysql.SMC_Data")
+relevant_merchants <- dbGetQuery(mydb, "SELECT distinct merchant_code from mysql.SMC_Data")
+merchant_metrics <- dbGetQuery(mydb, "SELECT * from mysql.cap1_disk1_merchant_metrics where merchant_code in 
+                  ('M1008','M1707','M1883','M2168','M3868')")
+merchant_type <- dbGetQuery(mydb, "SELECT distinct Industry_Name, merchant_code from mysql.cap1_disk4_validation_auth where merchant_code 
+                in ('M1008','M1707','M1883','M2168','M2203','M2493','M3123','M3126','M3172','M3342','M3437','M3456','M382','M3868')")
+
+merchant_type2 <- dbGetQuery(mydb, "SELECT distinct Industry_Name, merchant_code from mysql.cap1_disk1_build_auth_M155 where merchant_code 
+                in ('M1008','M1707','M1883','M2168','M2203','M2493','M3123','M3126','M3172','M3342','M3437','M3456','M382','M3868')")
+
+dbDisconnect(mydb)
