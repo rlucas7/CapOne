3188ec38805fccabb58e31157cc2b3d12a186196
andyhoegh <ahoegh@vt.edu>
2013-11-03 16:39:46 -0500

    My very last submission

--- Last_Combine.R
+++ Last_Combine.R
@@ -1,4 +1,5 @@
 out <- read.csv('~/CapOne/bigout.csv')
 out_little <- read.csv("~/Dropbox/littleout.csv")
-final_out <- rbind(out[,1],out_little[,-1])
+final_out <- rbind(out[,-1],out_little[,-1])
+final_out <- final_out[order(final_out$acct_id_code),]
 write.csv(final_out,'~/CapOne/SMC_Output_30.csv')
