5449af4739748940fb556670f8f17ccbc5d8406b
andyhoegh <ahoegh@vt.edu>
2013-11-04 08:44:43 -0500

    Delete Last_Combine.R

--- Last_Combine.R
+++
@@ -1,5 +0,0 @@
-out <- read.csv('~/CapOne/bigout.csv')
-out_little <- read.csv("~/Dropbox/littleout.csv")
-final_out <- rbind(out[,-1],out_little[,-1])
-final_out <- final_out[order(final_out$acct_id_code),]
-write.csv(final_out,'~/CapOne/SMC_Output_30.csv')
